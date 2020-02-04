cae706b40bb843015b44afa89eababf7a2d48fa3
Jiaming Shang <jiaming.shang@rea-group.com>
2020-02-04 22:14:00 +0800

    add more route

--- src/main/scala/com/http4sintro/Main.scala
+++ src/main/scala/com/http4sintro/Main.scala
@@ -7,12 +7,74 @@ import org.http4s.HttpApp
 import org.http4s.HttpRoutes
 import org.http4s.dsl.io._
 import org.http4s.implicits._
+import cats.data.Validated.Invalid
+import cats.data.Validated.Valid
+import io.circe.HCursor
+import io.circe.Decoder
+import io.circe.Encoder
+import io.circe.Json
+import io.circe.syntax._
+import org.http4s.EntityDecoder
+import org.http4s.circe._
+import org.http4s.EntityEncoder
 
 object Main extends IOApp {
+  object UserName extends QueryParamDecoderMatcher[String]("user_name")
+
+  object Age extends OptionalQueryParamDecoderMatcher[String]("age")
+
+  object PhoneNumber extends OptionalMultiQueryParamDecoderMatcher[String]("phone")
+
+  case class Person(name: String, age: Option[String], phoneNumbers: List[String])
+
+  object Person {
+    implicit val personDecoder: Decoder[Person] = new Decoder[Person] {
+      override def apply(c: HCursor): Decoder.Result[Person] =
+        for {
+          name <- c.get[String]("name")
+          age <- c.get[Option[String]]("age")
+          phoneNumbers <- c.get[List[String]]("phone")
+        } yield Person(name, age, phoneNumbers)
+    }
+
+    implicit val personEncoder: Encoder[Person] = new Encoder[Person] {
+      override def apply(a: Person): Json = Json.obj(
+        "name" -> a.name.asJson,
+        "age" -> a.age.asJson,
+        "phone" -> a.phoneNumbers.asJson
+      )
+    }
+
+    implicit val personEntityDecoder: EntityDecoder[IO, Person] = jsonOf[IO, Person]
+    implicit val personEntityEncoder: EntityEncoder[IO, Person] = jsonEncoderOf[IO, Person]
+
+  }
+
   def route: HttpApp[IO] =
     HttpRoutes
       .of[IO]({
-        case GET -> Root / "hello" => Ok("hello world")
+        case GET -> Root / "hello" / name => Ok(s"Hello, ${name}")
+        case GET -> Root / "hello" :? UserName(name) +& Age(age) +& PhoneNumber(phoneNumber) =>
+          phoneNumber match {
+            case Invalid(e) => BadRequest("Failed to parse phone number.")
+            case Valid(Nil) =>
+              age.fold(Ok(s"Hello, ${name}"))(x => Ok(s"Hello, ${name}, your age is ${x}"))
+            case Valid(numbers) =>
+              age.fold(Ok(s"Hello, ${name}, your phone number is ${numbers.mkString(";")}"))(
+                x => Ok(s"Hello, ${name}, your age is ${x}, your phone number is ${numbers.mkString(";")}")
+              )
+          }
+        case GET -> Root / "json" / "hello" :? UserName(name) +& Age(age) +& PhoneNumber(phoneNumber) =>
+          phoneNumber match {
+            case Invalid(e) => BadRequest("Failed to parse phone number.")
+            case Valid(numbers) =>
+              Ok(Person(name, age, numbers))
+          }
+        case request @ POST -> Root / "json" / "hello" =>
+          request.as[Person].flatMap(person => Ok(person))
+        case GET -> Root / "add" / "int" / IntVar(x) / IntVar(y)    => Ok(s"Result is ${x + y}")
+        case GET -> Root / "add" / "long" / LongVar(x) / LongVar(y) => Ok(s"Result is ${x + y}")
+        case request @ GET -> Root / "header"                       => Ok(request.headers.toString)
       })
       .orNotFound
   override def run(args: List[String]): IO[ExitCode] =
